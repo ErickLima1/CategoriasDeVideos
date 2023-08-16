4438714a1b3ddc39c976584a24e8e58ddeddaac7
ErickLima <ericklimacosta29@gmail.com>
2023-08-16 10:27:30 -0300

    Service Category Delete

---
+++ src/services/deleteCategoryServices.ts
@@ -0,0 +1,19 @@
+import { getRepository } from "typeorm";
+import { Category } from "../entities/category";
+
+export class DeleteCategoryService {
+    async execute(id: string) {
+        try {
+            const repo = getRepository(Category);
+
+            if(!(await repo.findOne(id))) {
+                return new Error("Categoria Não Existe!");
+            };
+
+            await repo.delete(id);
+            
+        } catch(error) {
+            return error;
+        };
+    };
+};
